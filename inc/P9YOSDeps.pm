cd1cac791fdf9f471c9314ce8ff5fdae73d4be8c
Brendan Byrd <Perl@ResonatorSoft.org>
2013-06-04 22:22:35 -0400

    Fix PREREQ_PM insertion to work with quotes

--- inc/P9YOSDeps.pm
+++ inc/P9YOSDeps.pm
@@ -14,60 +14,59 @@ override _build_WriteMakefile_dump => sub {
    my ($self) = @_;
    my $txt = super();
 
-   $txt =~ s/('PREREQ_PM' => \{)/$1\n    &os_deps,/g;
+   $txt =~ s/([\'\"]?PREREQ_PM[\'\"]? => \{)/$1\n    &os_deps,/g;
+   
    return $txt;
 };
 
 override _build_MakeFile_PL_template => sub {
-    my ($self) = @_;
-    my $template = super();
+   my ($self) = @_;
+   my $template = super();
 
-    $template .= <<'TEMPLATE';
-use v5.8.8;
-
+   $template .= <<'TEMPLATE';
 sub os_deps {
-    my $_os = lc($^O);
+   my $_os = lc($^O);
 
-    if ( $_os =~ /mswin32|cygwin/ ) {
+   if ( $_os =~ /mswin32|cygwin/ ) {
       return (
-            'Win32::Process'       => 0,
-            'Win32::Process::Info' => 0,
-            'Path::Class'          => 0.32,  # fixes Cygwin path issue
-        );
-    }
-    elsif ( $_os eq 'freebsd' ) {
+         'Win32::Process'       => 0,
+         'Win32::Process::Info' => 1.020,  # WMI on Cygwin
+         'Path::Class'          => 0.32,   # fixes Cygwin path issue
+      );
+   }
+   elsif ( $_os eq 'freebsd' ) {
       return ( 'BSD::Process' => 0 );
-    }
-    elsif ( $_os eq 'darwin' ) {
+   }
+   elsif ( $_os eq 'darwin' ) {
       return ( 'Proc::ProcessTable' => 0.45 );
-    }
-    elsif ( $_os eq 'os2' ) {
+   }
+   elsif ( $_os eq 'os2' ) {
       return ( 'OS2::Process' => 0 );
-    }
-    elsif ( $_os eq 'vms' ) {
+   }
+   elsif ( $_os eq 'vms' ) {
       return ( 'VMS::Process' => 0 );
-    }
-    elsif ( $_os eq 'dos' ) {
-        die "Heh, DOS processes... you're funny!";
-    }
-    else {
-        # let's hope they have /proc
-        if ( not -d '/proc' ) {
-            if ( $_os =~ /bsd|dragonfly/ ) {
-                die "BSD::Process only works for FreeBSD."
-                  . " Encourage development for $_os, or write your own and I can include it here.";
-            }
-            else {
-                die "No idea how to handle $_os processes."
-                  . " Email me with more information!";
-            }
-        }
-    }
-  return ();
+   }
+   elsif ( $_os eq 'dos' ) {
+      die "Heh, DOS processes... you're funny!";
+   }
+   else {
+      # let's hope they have /proc
+      if ( not -d '/proc' ) {
+         if ( $_os =~ /bsd|dragonfly/ ) {
+            die "BSD::Process only works for FreeBSD."
+              . " Encourage development for $_os, or write your own and I can include it here.";
+         }
+         else {
+            die "No idea how to handle $_os processes."
+              . " Email me with more information!";
+         }
+      }
+   }
+   return ();
 }
 TEMPLATE
 
-    return $template;
+   return $template;
 };
 
 __PACKAGE__->meta->make_immutable;
